9bdb1a8945d4ef9d1954ce143f3711123eb06961
EduardoSA1999 <eduardoamaral1914@gmail.com>
2024-05-25 16:18:08 -0300

    Add comprar pista

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
     let qtd = document.getElementById('qtd');
 
-    alert(tipo.value);
-    alert(qtd.value);
+    if(tipo.value == 'pista') {
+        comprarPista(qnt.value);
+    }
+}
+
+function comprarPista(qnt) {
+    let qtdPista = document.getElementById('qtd-pista');
+    if (qtd > qtdPista.textContent) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        qtdPista.textContent = qtdPista;
+        alert('Compra realizado com sucesso!');
+    }
 }
